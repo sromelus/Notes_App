4066e9962763b292576a168933efe8d78d6ef285
Shardly Romelus <rshardlypicard89@gmail.com>
2021-05-11 17:24:21 -0400

    add note with user associations and note model test

---
+++ app/models/note.rb
@@ -0,0 +1,12 @@
+class Note < ApplicationRecord
+    validates :user_id, :title, presence: true
+    validates :title, length: { maximum: 30 }
+    validates :body, length: { maximum: 1000 }
+  
+    belongs_to :user
+  
+    def day_time
+      created_at.strftime("%a, %B %d at %I:%M %p")
+    end
+end
+  
