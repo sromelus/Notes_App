a1d0be3a9dc824d988dd20d604e538b992f27360
Shardly Romelus <rshardlypicard89@gmail.com>
2021-05-11 19:43:47 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
 
 
-
-5.times do |i|
-    User.create(
-        first_name: "fname#{i}",
-        last_name: "lname#{i}",
-        email: "fname#{i}@gmail.com",
-        password: "password",
-        confirm_password: "password"
-    )
-end
+# 5.times do |i|
+#     User.create(
+#         first_name: "fname#{i}",
+#         last_name: "lname#{i}",
+#         email: "fname#{i}@gmail.com",
+#         password: "password",
+#         confirm_password: "password"
+#     )
+# end
 
 
 20.times do
