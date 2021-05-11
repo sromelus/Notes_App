84c8a8c18bdbf043a5062cb3dcab4b44a3cd50ec
Shardly Romelus <rshardlypicard89@gmail.com>
2021-05-11 15:44:22 -0400

    capybara Gemgile

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,8 @@ module NoteApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
