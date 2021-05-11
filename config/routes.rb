08c37d788c6d5123d3c3bbede3ac23596db2cd09
Shardly Romelus <rshardlypicard89@gmail.com>
2021-05-11 18:07:58 -0400

    all users loggin signup etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root 'sessions#login'
 
-  # get '/sessions/login', to: 'sessions#login', as: :login
-  # get '/sessions/signup', to: 'sessions#signup', as: :signup
-  # get '/sessions/new_session', to: 'sessions#login'
-  # get '/sessions/registration', to: 'sessions#signup'
-  # post '/sessions/new_session', to: 'sessions#new_session', as: :new_session
-  # post '/sessions/registration', to: 'sessions#registration', as: :registration
-  # delete '/sessions/signout', to: 'sessions#destroy_session', as: :signout
+  get '/sessions/login', to: 'sessions#login', as: :login
+  get '/sessions/signup', to: 'sessions#signup', as: :signup
+  get '/sessions/new_session', to: 'sessions#login'
+  get '/sessions/registration', to: 'sessions#signup'
+  post '/sessions/new_session', to: 'sessions#new_session', as: :new_session
+  post '/sessions/registration', to: 'sessions#registration', as: :registration
+  delete '/sessions/signout', to: 'sessions#destroy_session', as: :signout
 
-  # resources :notes, except: :show 
+  resources :notes, except: :show 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
